ae9cf2538f14803dfbe950acba14bb7e8a61ab4b
efraincenitsa-glitch <efraincenitsa@gmail.com>
2026-01-27 13:53:31 -0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function calcular(){
-  let total = 0;
-  document.querySelectorAll('input[placeholder="$"]').forEach(input=>{
-    total += Number(input.value || 0);
-  });
-  document.getElementById('total').textContent = '$' + total.toFixed(2);
-}
